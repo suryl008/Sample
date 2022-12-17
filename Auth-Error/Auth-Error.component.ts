e193dc88248ae8f5ee005df07410a1cc0c922445
suryl008 <suresh.pounraj@gmail.com>
2022-12-17 06:04:03 -0500

    Add all my files

---
+++ Auth-Error/Auth-Error.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-auth-error",
+    templateUrl: './Auth-Error.component.html',
+    styleUrls: ['./Auth-Error.component.css']
+})
+export class AuthErrorComponent implements OnInit {
+
+    constructor() { }
+    
+    ngOnInit(): void {
+    }
+}
