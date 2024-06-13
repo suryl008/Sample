7afb51ee85de393722fd5618bbca088301b780f4
suryl008 <suresh.pounraj@gmail.com>
2024-06-13 13:37:46 -0400

    add files

--- Review/Setup/setup.component.spec.ts
+++ Review/Setup/setup.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QusPopupComponent } from './qus-popup.component';
+import { SetupComponent } from './setup.component';
 
-describe('QusPopupComponent', () => {
-  let component: QusPopupComponent;
-  let fixture: ComponentFixture<QusPopupComponent>;
+describe('SetupComponent', () => {
+  let component: SetupComponent;
+  let fixture: ComponentFixture<SetupComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QusPopupComponent ]
+      declarations: [ SetupComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(QusPopupComponent);
+    fixture = TestBed.createComponent(SetupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
